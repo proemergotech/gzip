da13223815e47032ef46665c403731885553ec4b
Gabor Pongracz <gabor.pongracz@proemergotech.com>
2021-05-14 14:51:11 +0200

    Update lib

--- gentlemangzip/gentleman_gzip.go
+++ gentlemangzip/gentleman_gzip.go
@@ -6,9 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"strings"
 
-	"gitlab.com/proemergotech/errors"
 	"gitlab.com/proemergotech/log-go/v3"
 
 	gcontext "gopkg.in/h2non/gentleman.v2/context"
@@ -16,65 +14,12 @@ import (
 )
 
 const (
-	HeaderAcceptEncoding  = "Accept-Encoding"
 	HeaderContentEncoding = "Content-Encoding"
 )
-
-// Response return a plugin which will add `Accept-Encoding: gzip` header to the request and decompress the gzip content of the response, if Content-Encoding header is exist and value is gzip.
-func Response(logger log.Logger) plugin.Plugin {
-	handlers := plugin.Handlers{}
-
-	handlers["before dial"] = func(gCtx *gcontext.Context, h gcontext.Handler) {
-		gCtx.Request.Header.Set(HeaderAcceptEncoding, "gzip")
-		h.Next(gCtx)
-	}
-
-	handlers["after dial"] = func(gCtx *gcontext.Context, h gcontext.Handler) {
-		if !strings.Contains(gCtx.Response.Header.Get(HeaderContentEncoding), "gzip") {
-			logger.Debug(gCtx, "no need for decompress, there is no content-encoding header with gzip value")
-			h.Next(gCtx)
-			return
-		}
-
-		if gCtx.Response == nil || gCtx.Response.Body == nil || gCtx.Response.Body == http.NoBody || gCtx.Response.ContentLength == 0 {
-			h.Next(gCtx)
-			return
-		}
-
-		var buf bytes.Buffer
-		gzr, err := cgzip.NewReader(gCtx.Response.Body)
-		if err != nil {
-			err = errors.Wrap(err, "cannot create response body reader")
-			logger.Error(gCtx, "decompression failed", "error", err)
-			h.Error(gCtx, err)
-			return
-		}
-		_, err = io.Copy(&buf, gzr) // #nosec
-		if err != nil {
-			err = errors.Wrap(err, "cannot decompress response body (copy)")
-			logger.Error(gCtx, "decompression failed", "error", err)
-			h.Error(gCtx, err)
-			return
-		}
-
-		if err := gzr.Close(); err != nil {
-			err = errors.Wrap(err, "cannot decompress response body (close)")
-			logger.Error(gCtx, "decompression failed", "error", err)
-			h.Error(gCtx, err)
-			return
-		}
-
-		gCtx.Response.Body = ioutil.NopCloser(&buf)
-		h.Next(gCtx)
-	}
-
-	return &plugin.Layer{Handlers: handlers}
-}
 
 // Request return a plugin which will compress body to gzip content and add the `Content-Encoding: gzip` header to the request.
 func Request(logger log.Logger) plugin.Plugin {
 	return plugin.NewRequestPlugin(func(gCtx *gcontext.Context, h gcontext.Handler) {
-
 		if gCtx.Request == nil || gCtx.Request.Method == "GET" || gCtx.Request.Body == nil || gCtx.Request.Body == http.NoBody || gCtx.Request.ContentLength == 0 {
 			h.Next(gCtx)
 			return
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module gitlab.com/proemergotech/gzip
 
-go 1.15
+go 1.16
 
 require (
+	github.com/labstack/echo/v4 v4.2.2
 	gitlab.com/proemergotech/errors v1.0.0
 	gitlab.com/proemergotech/log-go/v3 v3.0.3
 	gopkg.in/h2non/gentleman.v2 v2.0.4
